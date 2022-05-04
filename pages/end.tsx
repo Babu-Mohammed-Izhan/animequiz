9c6866db1a37697d440d0221b97e405368ee1a27
Izhan <babuizhan1234@gmail.com>
2022-05-04 19:56:26 +0530

    Added all the various pages to the quiz

---
+++ pages/end.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const End = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default End;
---
+++ pages/levels/[name].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Levels = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Levels;
