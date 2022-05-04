27a1ee2b5a953fa87fbea5c5d139837cef31f728
Izhan <babuizhan1234@gmail.com>
2022-05-04 20:39:58 +0530

    finished starting page

---
+++ pages/rules.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rules = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Rules;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Quicksand': ['Quicksand', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
