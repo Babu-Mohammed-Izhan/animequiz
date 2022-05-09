eb1c0704dea46903a6961f904f4db730dd2972d1
Izhan <babuizhan1234@gmail.com>
2022-05-09 21:15:31 +0530

    Designed the level cards

--- components/Levelcards.tsx
+++ components/Levelcards.tsx
@@ -13,8 +13,10 @@ interface levelTypes {
 const Levelcards = ({ name, colors, route }: levelTypes) => {
   return (
     <Link href={`${route}`} passHref>
-      <div className={`bg-gradient-to-tr from-${colors.from} to-${colors.to}`}>
-        <h1>{name}</h1>
+      <div
+        className={`bg-gradient-to-tr ${colors.from} ${colors.to} rounded-3xl pl-5 pr-44 pt-12 pb-5 w-full mt-16`}
+      >
+        <h1 className="text-white font-Quicksand text-4xl">{name}</h1>
       </div>
     </Link>
   );
--- pages/levels/index.tsx
+++ pages/levels/index.tsx
@@ -1,12 +1,35 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
+import Levelcards from '../../components/Levelcards';
 
 const StartPage = () => {
   return (
-    <div className="flex items-start justify-start flex-col min-h-screen p-20">
+    <div className="flex items-start justify-start flex-col min-h-screen w-11/12 mx-auto py-20">
       <h1 className="text-6xl font-medium text-transparent bg-clip-text bg-gradient-to-tr from-indigo-700 to-violet-500">
         Let's Play
       </h1>
+      <div className="grid lg:grid-cols-3 grid-cols-1 gap-3 w-11/12 mx-auto">
+        <Levelcards
+          name="Basic"
+          colors={{ from: 'from-blue-500', to: 'to-blue-400' }}
+          route="/game/basic"
+        />
+        <Levelcards
+          name="Basic"
+          colors={{ from: 'from-blue-500', to: 'to-blue-400' }}
+          route="/game/basic"
+        />
+        <Levelcards
+          name="Basic"
+          colors={{ from: 'from-blue-500', to: 'to-blue-400' }}
+          route="/game/basic"
+        />
+        <Levelcards
+          name="Basic"
+          colors={{ from: 'from-blue-500', to: 'to-blue-400' }}
+          route="/game/basic"
+        />
+      </div>
     </div>
   );
 };
