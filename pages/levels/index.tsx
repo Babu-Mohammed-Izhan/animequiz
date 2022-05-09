a48961e22b2f6556484e1efa5e00ed5c2500ad62
Izhan <babuizhan1234@gmail.com>
2022-05-09 21:21:09 +0530

    added levels

--- pages/levels/index.tsx
+++ pages/levels/index.tsx
@@ -5,7 +5,7 @@ import Levelcards from '../../components/Levelcards';
 const StartPage = () => {
   return (
     <div className="flex items-start justify-start flex-col min-h-screen w-11/12 mx-auto py-20">
-      <h1 className="text-6xl font-medium text-transparent bg-clip-text bg-gradient-to-tr from-indigo-700 to-violet-500">
+      <h1 className="text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-indigo-700 to-violet-500">
         Let's Play
       </h1>
       <div className="grid lg:grid-cols-3 grid-cols-1 gap-3 w-11/12 mx-auto">
@@ -13,21 +13,25 @@ const StartPage = () => {
           name="Basic"
           colors={{ from: 'from-blue-500', to: 'to-blue-400' }}
           route="/game/basic"
+          level="1"
         />
         <Levelcards
           name="Basic"
           colors={{ from: 'from-blue-500', to: 'to-blue-400' }}
           route="/game/basic"
+          level="2"
         />
         <Levelcards
           name="Basic"
           colors={{ from: 'from-blue-500', to: 'to-blue-400' }}
           route="/game/basic"
+          level="3"
         />
         <Levelcards
           name="Basic"
           colors={{ from: 'from-blue-500', to: 'to-blue-400' }}
           route="/game/basic"
+          level="4"
         />
       </div>
     </div>
