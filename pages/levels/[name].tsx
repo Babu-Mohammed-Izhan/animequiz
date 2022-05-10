348e282e70e756c5b7ef19b28658606de344e57a
Izhan <babuizhan1234@gmail.com>
2022-05-10 17:10:29 +0530

    added game page

--- pages/levels/[name].tsx
+++ pages/levels/[name].tsx
@@ -10,18 +10,18 @@ const Levels = ({ name }: gameType) => {
 
   return (
     <div className="bg-gradient-to-tr from-blue-600 to-teal-500 h-screen font-Quicksand">
-      <div className="flex flex-col items-start justify-between h-full w-11/12 mx-auto pt-20 pb-48">
+      <div className="flex flex-col items-start justify-between h-full w-10/12 mx-auto pt-20 pb-48">
         <button
-          onClick={() => router.push('/')}
+          onClick={() => router.back()}
           className="rounded-full px-[10px] py-[6px] bg-transparent border-2 border-white"
         >
           <i className="fa-solid fa-xmark h-4 w-4 text-white"></i>
         </button>
         <div className="w-full">
           <h3 className="text-white text-3xl">level 2</h3>
-          <h1 className="text-white text-6xl mb-40">{name}</h1>
-          <button className="bg-white rounded-3xl w-full mx-auto py-5 ">
-            <span className="text-transparent bg-clip-text bg-gradient-to-tr from-blue-600 to-teal-500 font-bold text-2xl">
+          <h1 className="text-white text-6xl mb-40 capitalize">{name}</h1>
+          <button className="bg-white rounded-3xl w-full mx-auto py-5 shadow-xl">
+            <span className="text-transparent bg-clip-text bg-gradient-to-tr from-blue-600 to-teal-500 font-bold text-3xl">
               Game
             </span>
           </button>
