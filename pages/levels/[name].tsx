760c3c6f2e3c9b62b0f5a8fea1a4339e1481f04f
Izhan <babuizhan1234@gmail.com>
2022-05-10 16:49:05 +0530

    designed game page

--- pages/levels/[name].tsx
+++ pages/levels/[name].tsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 interface gameType {
   name: string;
 }
 
 const Levels = ({ name }: gameType) => {
+  const router = useRouter();
+
   return (
-    <div className="bg-gradient-to-tr from-blue-600 to-teal-500 h-screen flex items-center justify-center">
-      <h3>level 2</h3>
-      <h1>{name}</h1>
-      <button>Game</button>
+    <div className="bg-gradient-to-tr from-blue-600 to-teal-500 h-screen font-Quicksand">
+      <div className="flex flex-col items-start justify-between h-full w-11/12 mx-auto pt-20 pb-48">
+        <button
+          onClick={() => router.push('/')}
+          className="rounded-full px-[10px] py-[6px] bg-transparent border-2 border-white"
+        >
+          <i className="fa-solid fa-xmark h-4 w-4 text-white"></i>
+        </button>
+        <div className="w-full">
+          <h3 className="text-white text-3xl">level 2</h3>
+          <h1 className="text-white text-6xl mb-40">{name}</h1>
+          <button className="bg-white rounded-3xl w-full mx-auto py-5 ">
+            <span className="text-transparent bg-clip-text bg-gradient-to-tr from-blue-600 to-teal-500 font-bold text-2xl">
+              Game
+            </span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
