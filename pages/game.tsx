5fc205f64fefa6aa34b3f385b681c10086beb941
Izhan <babuizhan1234@gmail.com>
2022-05-11 22:33:25 +0530

    Added styles to game page

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,16 +1,30 @@
 import React, { EventHandler, useEffect, useState } from 'react';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 import { Question } from '../types';
 
 const Game = () => {
+  const router = useRouter();
+
   const [options, setOptions] = useState<String[]>([]);
   const [score, setScore] = useState(0);
   const [number, setNumber] = useState(0);
   const [visible, setVisible] = useState(false);
-  const [ques, setques] = useState<Question[]>([]);
+  const [ques, setQues] = useState<Question[]>([]);
   const [questionnum, setQuestionnum] = useState(1);
   const [redirectboard, setredirectboard] = useState(false);
   const [alert, setAlert] = useState('');
+  const [health, setHealth] = useState(3);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get('');
+      setQues(data.data.results);
+      randomizeans(data.data.results[0]);
+      setVisible(true);
+    };
+    getData();
+  }, []);
 
   const randomizeans = (question: Question) => {
     const answers = [question.op1, question.op2, question.op3, question.op4];
@@ -47,19 +61,50 @@ const Game = () => {
 
   return (
     <div>
+      <nav>
+        <button
+          onClick={() => router.push('/')}
+          className="rounded-full px-2 py-[2px] bg-white"
+        >
+          <i className="fa-solid fa-xmark h-3 w-3"></i>
+        </button>
+        <p>{questionnum}</p>
+        <div>{health}</div>
+      </nav>
       <h4>question {questionnum} of 10</h4>
+      <h1>{ques[number].q}</h1>
       <div>
-        <button onClick={() => handleAnswerOptionClick(options[0])}>
-          {options[0]}
+        <button
+          className="bg-white rounded-3xl w-full mx-auto py-5 shadow-xl"
+          onClick={() => handleAnswerOptionClick(options[0])}
+        >
+          <span className="text-transparent bg-clip-text bg-gradient-to-tr from-blue-600 to-teal-500 font-bold text-3xl">
+            {options[0]}
+          </span>
         </button>
-        <button onClick={() => handleAnswerOptionClick(options[1])}>
-          {options[1]}
+        <button
+          className="bg-white rounded-3xl w-full mx-auto py-5 shadow-xl"
+          onClick={() => handleAnswerOptionClick(options[1])}
+        >
+          <span className="text-transparent bg-clip-text bg-gradient-to-tr from-blue-600 to-teal-500 font-bold text-3xl">
+            {options[1]}
+          </span>
         </button>
-        <button onClick={() => handleAnswerOptionClick(options[2])}>
-          {options[2]}
+        <button
+          className="bg-white rounded-3xl w-full mx-auto py-5 shadow-xl"
+          onClick={() => handleAnswerOptionClick(options[2])}
+        >
+          <span className="text-transparent bg-clip-text bg-gradient-to-tr from-blue-600 to-teal-500 font-bold text-3xl">
+            {options[2]}
+          </span>
         </button>
-        <button onClick={() => handleAnswerOptionClick(options[3])}>
-          {options[3]}
+        <button
+          className="bg-white rounded-3xl w-full mx-auto py-5 shadow-xl"
+          onClick={() => handleAnswerOptionClick(options[3])}
+        >
+          <span className="text-transparent bg-clip-text bg-gradient-to-tr from-blue-600 to-teal-500 font-bold text-3xl">
+            {options[3]}
+          </span>
         </button>
       </div>
     </div>
